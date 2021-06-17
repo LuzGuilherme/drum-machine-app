99624fb3a4c7b9df91414fa4535a56be8a13f2da
Guilherme Duarte da Luz <gduartedaluz@gmail.com>
2021-06-17 16:51:38 +0100

    g

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { Component } from "react";
-import ReactHowler from "react-howler";
 import DrumPad from "./Clips";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-
-         }
-
-    }
 
     render() { 
         return ( 
             <div>
+                <div className="title">
+                    <h1>Drum Machine</h1>
+                </div>
                 <DrumPad />
             </div>
         );
